dd088a5add98426e0b4f6e221ae6a0f350d0b5cf
mostafa-khodaparast <mostafa.khodaparast1997@gmail.com>
2023-09-18 15:49:05 +0330

    create starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,24 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
-
-import CityList from "./components/CityList";
-import CountryList from "./components/CountryList";
-import City from "./components/City";
-import Form from "./components/Form";
-import SpinnerFullPage from "./components/SpinnerFullPage";
+import styled from 'styled-components'
 
 
-const Homepage = lazy(() => import("./pages/Homepage"));
-const Product = lazy(() => import("./pages/Product"));
-const Pricing = lazy(() => import("./pages/Pricing"));
-const Login = lazy(() => import("./pages/Login"));
-const AppLayout = lazy(() => import("./pages/AppLayout"));
-const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const H1 = styled.h1`
+    font-size: 15px
+  `;
 
+const Button = styled.button`
+  font-size: 30px;
+  background-color: blue;
+  color: white;
+`;
 
 function App() {
+
   return (
-    <AuthProvider>
-      <CitiesProvider>
-        <BrowserRouter>
-          <Suspense fallback={<SpinnerFullPage />}>
-            <Routes>
-              <Route index element={<Homepage />} />
-              <Route path="product" element={<Product />} />
-              <Route path="pricing" element={<Pricing />} />
-              <Route path="login" element={<Login />} />
-              <Route path="app" element={<ProtectedRoute><AppLayout /></ProtectedRoute>}>
-                <Route index element={<Navigate to="cities" replace />} />
-                <Route path="cities" element={<CityList />} />
-                <Route path="cities/:id" element={<City />} />
-                <Route path="countries" element={<CountryList />} />
-                <Route path="form" element={<Form />} />
-              </Route>
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
-          </Suspense>
-        </BrowserRouter>
-      </CitiesProvider>
-    </AuthProvider>
-  );
+    <div>
+      <H1>mostafa</H1>
+      <Button onClick={()=> console.log("hiclicked")}>click</Button>
+    </div>
+    )
 }
 
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,63 +1,17 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
-import Button from "../components/Button";
-import PageNav from "../components/PageNav";
+const LoginLayout = styled.main`
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: 48rem;
+  align-content: center;
+  justify-content: center;
+  gap: 3.2rem;
+  background-color: var(--color-grey-50);
+`;
 
-import { useAuth } from "../contexts/FakeAuthContext";
-import styles from "./Login.module.css";
-
-
-const Login = () => {
-  const [email, setEmail] = useState("mostafa.khodaparast1997@gmail.com");
-  const [password, setPassword] = useState("1234");
-  const { login, isAuthenticated, error } = useAuth();
-  const navigate = useNavigate();
-
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (email && password) login(email, password);
-  }
-
-
-  useEffect(() => {
-      if (isAuthenticated) navigate("/app", { replace: true });
-    }, [isAuthenticated, navigate]);
-
-
-  return (
-    <main className={styles.login}>
-      <PageNav />
-
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.row}>
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </div>
-
-        <div className={styles.row}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-        </div>
-
-        <div>
-          <Button type="primary">Login</Button>
-          {error ? <p style={{fontSize: '1.4rem'}}>{error}</p> : null}
-        </div>
-      </form>
-    </main>
-  );
+function Login() {
+  return <LoginLayout>Login</LoginLayout>;
 }
 
-export default Login
+export default Login;
